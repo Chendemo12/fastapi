05a5c4339a7331f26041d4524978a329398c7d68
lee <ithika@163.com>
2023-12-02 21:47:15 +0800

    feat: add pathschema

--- pathschema/pathschema.go
+++ pathschema/pathschema.go
@@ -1,8 +1,9 @@
 package pathschema
 
 import (
+	"regexp"
 	"strings"
-	"sync"
+	"unicode"
 )
 
 const (
@@ -11,14 +12,35 @@ const (
 	OptionalQueryParamPrefix = "?" // 查询参数起始字符,也是路径参数结束字符
 )
 
+var rule = regexp.MustCompile(`[A-Z][a-z]*`)
+
+// Default 默认路由解析方法, 全小写-短横线
+func Default() RoutePathSchema { return LowerCaseDash }
+
+// LowerCaseDash 全小写-短横线
+var LowerCaseDash = NewComposition(&LowerCase{}, &UnixDash{})
+
+// LowerCaseBackslash 全小写段路由
+var LowerCaseBackslash = NewComposition(&LowerCase{}, &Backslash{})
+
 // RoutePathSchema 路由格式化方案
 type RoutePathSchema interface {
 	Name() string                       // 路由名称
-	Connector() string                  // 路径连接符
-	Split(relativePath string) []string // 将相对路由分组
+	Connector() string                  // 路径连接符，仅对分组后的相对路由有效，用于将分段后的相对路由组合为一个完成的路由
+	Split(relativePath string) []string // 将相对路由分段，relativePath 为去除 Http.Method 后的方法名
 }
 
+// Dash 短横线
+type Dash UnixDash
+
 // LowerCamelCase 小驼峰
+// 将结构体名称按单词分割后转换为小驼峰的形式后作为相对路由
+//
+//	# example
+//
+//	GetClipboardContent()	=> /clipboardContent
+//	ClipSettingsPost()		=> /clipSettings
+//	QueryTextHistoryGet()	=> /queryTextHistory
 type LowerCamelCase struct{}
 
 func (s LowerCamelCase) Name() string { return "LowerCamelCase" }
@@ -26,45 +48,93 @@ func (s LowerCamelCase) Name() string { return "LowerCamelCase" }
 func (s LowerCamelCase) Connector() string { return "" }
 
 func (s LowerCamelCase) Split(relativePath string) []string {
-	return []string{}
+	return []string{LowercaseFirstLetter(relativePath)}
 }
 
-// UpperCamelCase 大驼峰
-type UpperCamelCase struct{}
+// LowerCase 全小写字符
+// 将方法名按单词分割后全部转换为小写字符再直接拼接起来
+//
+//	# example
+//
+//	GetClipboardContent()	=> /clipboardcontent
+//	ClipSettingsPost()		=> /clipsettings
+//	QueryTextHistoryGet()	=> /querytexthistory
+type LowerCase struct{}
 
-func (s UpperCamelCase) Name() string { return "UpperCamelCase" }
+func (s LowerCase) Name() string { return "LowerCase" }
 
-func (s UpperCamelCase) Connector() string { return "" }
+func (s LowerCase) Connector() string { return "" }
 
-func (s UpperCamelCase) Split(relativePath string) []string {
-	return []string{}
+func (s LowerCase) Split(relativePath string) []string {
+	spans := SplitWords(relativePath)
+	for i := 0; i < len(spans); i++ {
+		spans[i] = LowercaseFirstLetter(spans[i])
+	}
+	return spans
 }
-
-// Dash 短横线
-type Dash UnixDash
 
 // UnixDash 短横线
+// 将方法名按单词分割后用"-"相连接
+//
+//	# example
+//
+//	GetClipboardContent()	=> /Clipboard-Content
+//	ClipSettingsPost()		=> /Clip-Settings
+//	QueryTextHistoryGet()	=> /Query-Text-History
 type UnixDash struct{}
 
 func (s UnixDash) Name() string { return "UnixDash" }
 
-func (s UnixDash) Connector() string { return "" }
+func (s UnixDash) Connector() string { return "-" }
 
 func (s UnixDash) Split(relativePath string) []string {
-	return []string{}
+	return SplitWords(relativePath)
+}
+
+// Underline 下划线
+// 将方法名按单词分割后用"_"相连接
+//
+//	# example
+//
+//	GetClipboardContent()	=> /Clipboard-Content
+//	ClipSettingsPost()		=> /Clip-Settings
+//	QueryTextHistoryGet()	=> /Query-Text-History
+type Underline struct{}
+
+func (s Underline) Name() string { return "Underline" }
+
+func (s Underline) Connector() string { return "_" }
+
+func (s Underline) Split(relativePath string) []string {
+	return SplitWords(relativePath)
 }
 
 // Backslash 反斜杠
+// 按单词分段，每一个单词都作为一个路由段
+//
+//	# example
+//
+//	GetClipboardContent()	=> /Clipboard/Content
+//	ClipSettingsPost()		=> /Clip/Settings
+//	QueryTextHistoryGet()	=> /Query/Text/History
 type Backslash struct{}
 
 func (s Backslash) Name() string { return "Backslash" }
 
-func (s Backslash) Connector() string { return "" }
+func (s Backslash) Connector() string { return PathSeparator }
+
 func (s Backslash) Split(relativePath string) []string {
-	return []string{}
+	return SplitWords(relativePath)
 }
 
-// Original 原始不变，保持结构体方法名,只拼接成合法的路由
+// Original 原始不变，保持结构体方法名(不含HTTP方法名),只拼接成合法的路由
+// 由于结构体非导出方法不会作为路由函数处理，因此此方案等同于大驼峰形式
+//
+//	# example
+//
+//	GetClipboardContent()	=> /ClipboardContent
+//	ClipSettingsPost()		=> /ClipSettings
+//	QueryTextHistoryGet()	=> /QueryTextHistory
 type Original struct{}
 
 func (s Original) Name() string { return "Original" }
@@ -73,6 +143,42 @@ func (s Original) Connector() string { return "" }
 
 func (s Original) Split(relativePath string) []string {
 	return []string{relativePath}
+}
+
+// AddPrefix 用于在分段路由基础上添加一个前缀字符，作用于每一段路径，通常与其他方案组合使用
+type AddPrefix struct {
+	Prefix string
+}
+
+func (s AddPrefix) Name() string { return "AddPrefix" }
+
+func (s AddPrefix) Connector() string { return "" }
+
+func (s AddPrefix) Split(relativePath string) []string {
+	spans := SplitWords(relativePath)
+	for i := 0; i < len(spans); i++ {
+		spans[i] = s.Prefix + spans[i]
+	}
+
+	return spans
+}
+
+// AddSuffix 用于在分段路由基础上添加一个后缀字符，作用于每一段路径，通常与其他方案组合使用
+type AddSuffix struct {
+	Suffix string
+}
+
+func (s AddSuffix) Name() string { return "AddSuffix" }
+
+func (s AddSuffix) Connector() string { return "" }
+
+func (s AddSuffix) Split(relativePath string) []string {
+	spans := SplitWords(relativePath)
+	for i := 0; i < len(spans); i++ {
+		spans[i] = spans[i] + s.Suffix
+	}
+
+	return spans
 }
 
 // Composition 组合式路由格式化方案, 通过按顺序执行多个 RoutePathSchema 获得最终路由
@@ -88,6 +194,8 @@ func (s Original) Split(relativePath string) []string {
 //  3. 继续迭代 spans 的元素, 重复步骤2得到 Sx
 //
 //  4. Format 方法会将 S1 ~ Sx 以 Connector 为连接符组合起来得到最终的路由
+//
+// 如果格式化方案为空，则效果等同于 Original
 type Composition struct {
 	schemas []RoutePathSchema
 	linker  string
@@ -96,14 +204,6 @@ type Composition struct {
 func (s *Composition) Name() string { return "Composition" }
 
 func (s *Composition) Connector() string {
-	if s.linker == "" {
-		links := make([]string, len(s.schemas))
-		for index, schema := range s.schemas {
-			links[index] = schema.Connector()
-		}
-		s.linker = strings.Join(links, "")
-	}
-
 	return s.linker
 }
 
@@ -113,36 +213,24 @@ func (s *Composition) Split(relativePath string) []string {
 	}
 
 	spans := s.schemas[0].Split(relativePath)
-	for i := 0; i <= len(spans); i++ {
-		span := spans[i]
-		spans[i] = s.iter(span)
+	for i := 0; i < len(spans); i++ {
+		for _, schema := range s.schemas[0:] {
+			spans[i] = strings.Join(schema.Split(spans[i]), "")
+		}
 	}
 
 	return spans
 }
 
-func (s *Composition) next(relativePath string, index int) string {
-	ss := s.schemas[index+1].Split(relativePath)
-	return strings.Join(ss, "")
-}
-
-func (s *Composition) iter(relativePath string) string {
-	span := ""
-
-	// TODO: wrong
-	var ss []string
-	// 必须>1才有意义
-	for i := 1; i < len(s.schemas)-1; i++ {
-		ss = s.schemas[i].Split(relativePath)
-		span = s.next(strings.Join(ss, ""), i)
-	}
-
-	return span
-}
-
 func NewComposition(schemas ...RoutePathSchema) *Composition {
-	schema := &Composition{schemas: schemas}
-	return schema
+	schema_ := &Composition{schemas: schemas}
+	links := make([]string, len(schemas))
+	for index, s := range schemas {
+		links[index] = s.Connector()
+	}
+	schema_.linker = strings.Join(links, "")
+
+	return schema_
 }
 
 // Format 按照方案格式化并组合路由
@@ -160,24 +248,40 @@ func Format(prefix string, relativePath string, schema RoutePathSchema) string {
 	}
 
 	spans := schema.Split(relativePath)
-	for _, span := range spans {
-		full += span
-		full += schema.Connector()
-	}
 
-	return full
+	return full + strings.Join(spans, schema.Connector())
 }
 
-var schema RoutePathSchema
-var one = &sync.Once{}
+// SplitWords 将字符串s按照单词进行切分, 判断单词的依据为：是否首字母大写
+// 如果输入s无法切分，则返回只有s构成的一个元素的数组
+func SplitWords(s string) []string {
+	spans := rule.FindAllString(s, -1)
+	if spans == nil {
+		spans = []string{s}
+	}
+	return spans
+}
 
-// Default 默认路由解析方法, 全小写-短横线
-func Default() RoutePathSchema {
-	one.Do(func() {
-		schema = &Composition{schemas: []RoutePathSchema{&LowerCamelCase{}, &UnixDash{}}}
-		// TODO:
-		schema = &Original{}
-	})
+// LowercaseFirstLetter 将一个字符串的首字母转换为小写形式
+func LowercaseFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
 
-	return schema
+	r := []rune(s)
+	r[0] = unicode.ToLower(r[0])
+
+	return string(r)
+}
+
+// UppercaseFirstLetter 将一个字符串的首字母转换为大写形式
+func UppercaseFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	r := []rune(s)
+	r[0] = unicode.ToTitle(r[0])
+
+	return string(r)
 }
---
+++ pathschema/pathschema_test.go
@@ -0,0 +1,708 @@
+package pathschema
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBackslash_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "/",
+			want: "/",
+		},
+		{
+			name: "/",
+			want: "/",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Backslash{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackslash_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Backslash",
+			want: "Backslash",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Backslash{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBackslash_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"Read", "Proc", "Tree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Backslash{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCamelCase_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "",
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCamelCase{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCamelCase_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "LowerCamelCase",
+			want: "LowerCamelCase",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCamelCase{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCamelCase_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"readProcTree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCamelCase{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCase_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "",
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCase{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCase_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "LowerCase",
+			want: "LowerCase",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCase{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowerCase_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"read", "proc", "tree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := LowerCase{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOriginal_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "",
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Original{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOriginal_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Original",
+			want: "Original",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Original{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOriginal_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"ReadProcTree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Original{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnderline_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "_",
+			want: "_",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Underline{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnderline_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "Underline",
+			want: "Underline",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Underline{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnderline_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"Read", "Proc", "Tree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Underline{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnixDash_Connector(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "-",
+			want: "-",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := UnixDash{}
+			if got := s.Connector(); got != tt.want {
+				t.Errorf("Connector() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnixDash_Name(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "UnixDash",
+			want: "UnixDash",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := UnixDash{}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnixDash_Split(t *testing.T) {
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "ReadProcTree",
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"Read", "Proc", "Tree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := UnixDash{}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestComposition_Name(t *testing.T) {
+	type fields struct {
+		schemas []RoutePathSchema
+		linker  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name:   "Composition",
+			fields: fields{},
+			want:   "Composition",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Composition{
+				schemas: tt.fields.schemas,
+				linker:  tt.fields.linker,
+			}
+			if got := s.Name(); got != tt.want {
+				t.Errorf("Name() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestComposition_Split(t *testing.T) {
+	type fields struct {
+		schemas []RoutePathSchema
+		linker  string
+	}
+	type args struct {
+		relativePath string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []string
+	}{
+		{
+			name: "ReadProcTree",
+			fields: fields{
+				schemas: []RoutePathSchema{&LowerCase{}, &UnixDash{}},
+				linker:  "-",
+			},
+			args: args{
+				relativePath: "ReadProcTree",
+			},
+			want: []string{"read", "proc", "tree"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Composition{
+				schemas: tt.fields.schemas,
+				linker:  tt.fields.linker,
+			}
+			if got := s.Split(tt.args.relativePath); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Split() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUppercaseFirstLetter(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "upperPrice",
+			args: args{
+				s: "upperPrice",
+			},
+			want: "UpperPrice",
+		},
+		{
+			name: "HaJiMi",
+			args: args{
+				s: "HaJiMi",
+			},
+			want: "HaJiMi",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UppercaseFirstLetter(tt.args.s); got != tt.want {
+				t.Errorf("UppercaseFirstLetter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSplitWords(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "IsPreventCheat",
+			args: args{
+				s: "IsPreventCheat",
+			},
+			want: []string{"Is", "Prevent", "Cheat"},
+		},
+		{
+			name: "HaJiMi",
+			args: args{
+				s: "HaJiMi",
+			},
+			want: []string{"Ha", "Ji", "Mi"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SplitWords(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SplitWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLowercaseFirstLetter(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "LowerPrice",
+			args: args{
+				s: "LowerPrice",
+			},
+			want: "lowerPrice",
+		},
+		{
+			name: "HaJiMi",
+			args: args{
+				s: "HaJiMi",
+			},
+			want: "haJiMi",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LowercaseFirstLetter(tt.args.s); got != tt.want {
+				t.Errorf("LowercaseFirstLetter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFormat(t *testing.T) {
+	type args struct {
+		prefix       string
+		relativePath string
+		schema       RoutePathSchema
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "UnixDash",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &UnixDash{},
+			},
+			want: "/api/Read-Unix-Proc-Tree",
+		},
+		{
+			name: "Underline",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &Underline{},
+			},
+			want: "/api/Read_Unix_Proc_Tree",
+		},
+		{
+			name: "LowerCamelCase",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &LowerCamelCase{},
+			},
+			want: "/api/readUnixProcTree",
+		},
+		{
+			name: "LowerCase",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &LowerCase{},
+			},
+			want: "/api/readunixproctree",
+		},
+		{
+			name: "Backslash",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &Backslash{},
+			},
+			want: "/api/Read/Unix/Proc/Tree",
+		},
+		{
+			name: "Original",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       &Original{},
+			},
+			want: "/api/ReadUnixProcTree",
+		},
+		{
+			name: "LowerCaseDash",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       Default(),
+			},
+			want: "/api/read-unix-proc-tree",
+		},
+		{
+			name: "LowerCaseUnderline",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       NewComposition(&LowerCase{}, &Underline{}),
+			},
+			want: "/api/read_unix_proc_tree",
+		},
+		{
+			name: "LowerCaseDashUnderline",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       NewComposition(&LowerCase{}, &UnixDash{}, &Underline{}),
+			},
+			want: "/api/read-_unix-_proc-_tree",
+		},
+		{
+			name: "LowerCaseDashUnderlineBackslash",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       NewComposition(NewComposition(&LowerCase{}, &UnixDash{}), &Underline{}, &Backslash{}),
+			},
+			want: "/api/read-_/unix-_/proc-_/tree",
+		},
+		{
+			name: "LowerCaseWithPreEqual",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       NewComposition(&LowerCase{}, &AddPrefix{Prefix: "="}, &Backslash{}),
+			},
+			want: "/api/=read/=unix/=proc/=tree",
+		},
+		{
+			name: "LowerCaseWithPostDash",
+			args: args{
+				prefix:       "/api",
+				relativePath: "ReadUnixProcTree",
+				schema:       NewComposition(&LowerCase{}, &AddSuffix{Suffix: "-"}, &Backslash{}),
+			},
+			want: "/api/read-/unix-/proc-/tree-",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Format(tt.args.prefix, tt.args.relativePath, tt.args.schema); got != tt.want {
+				t.Errorf("Format() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
